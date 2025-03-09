3f7200554797a2952f43ed6c69c7b53774e50f62
sanket1305 <meetsanket24@gmail.com>
2025-03-09 00:10:48 -0500

    Task 4 done

--- data_wrangling/labs/code/task4.py
+++ data_wrangling/labs/code/task4.py
@@ -12,23 +12,23 @@ def plot(model, train_data, missing_data, result_df, predicted_values):
     ax = fig.add_subplot(111, projection='3d')
 
     # Plot existing data points
-    ax.scatter(train_data['days_since_prior_order'], train_data['item_count'], train_data['checkout_value'], color='blue', label='Existing Data')
+    ax.scatter(train_data['days_since_prior_order'], train_data['number_of_items'], train_data['checkout_value'], color='blue', label='Existing Data')
 
     # Plot predicted missing values
-    ax.scatter(missing_data['days_since_prior_order'], missing_data['item_count'], predicted_values, color='red', label='Predicted Data')
+    ax.scatter(missing_data['days_since_prior_order'], missing_data['number_of_items'], predicted_values, color='red', label='Predicted Data')
 
     # Create a mesh grid for the regression plane
     A_mesh, B_mesh = np.meshgrid(np.linspace(result_df['days_since_prior_order'].min(), result_df['days_since_prior_order'].max(), 50),
-                                np.linspace(result_df['item_count'].min(), result_df['item_count'].max(), 50))
+                                np.linspace(result_df['number_of_items'].min(), result_df['number_of_items'].max(), 50))
     C_mesh = model.predict(pd.DataFrame(np.c_[A_mesh.ravel(), B_mesh.ravel()],
-                                    columns=['days_since_prior_order', 'item_count'])).reshape(A_mesh.shape)
+                                    columns=['days_since_prior_order', 'number_of_items'])).reshape(A_mesh.shape)
 
     # Plot the regression plane
     ax.plot_surface(A_mesh, B_mesh, C_mesh, alpha=0.4, color='cyan')
 
     # Set labels
     ax.set_xlabel('days_since_prior_order (Feature 1)')
-    ax.set_ylabel('item_count (Feature 2)')
+    ax.set_ylabel('number_of_items (Feature 2)')
     ax.set_zlabel('checkout_value (Target/Output)')
 
     # Add legend
@@ -48,7 +48,7 @@ def predict_checkout_value(user_id: int, number_of_items: int, days_since_prior_
     cursor.execute("SELECT order_id, count(*) FROM OrderProducts GROUP BY order_id")
     results = cursor.fetchall()
 
-    df_order_item_count = pd.DataFrame(results, columns=['order_id', 'item_count'])
+    df_order_item_count = pd.DataFrame(results, columns=['order_id', 'number_of_items'])
 
     # read csv file into dataframe
     df_orders = pd.read_csv("../output/task3_output.csv")
@@ -56,29 +56,30 @@ def predict_checkout_value(user_id: int, number_of_items: int, days_since_prior_
     # Merge the dataframes on the 'order_id' column
     result_df = pd.merge(df_orders, df_order_item_count, on='order_id', how='left')
 
+    # drop records with no prev_orders (number_of_items missing)
+    result_df = result_df.dropna(subset=['number_of_items'])
+
+    # missing data imputation for 'days_since_prior_order' column
     result_df['days_since_prior_order'] = result_df['days_since_prior_order'].fillna(0)
 
-    user_df = result_df[result_df['user_id'] == user_id]
+    # getting non missing checkout value rows
+    train_data = result_df[result_df['checkout_value'].notnull()]
+
+    # get user specific data
+    user_df = train_data[train_data['user_id'] == user_id]
 
     # Check if the DataFrame is empty
-    if not user_df.empty:
-        check_not_null = user_df[user_df['checkout_value'].notnull()]
-        if not check_not_null.empty:
-            result_df = user_df.copy()
-    
-    result_df['item_count'] = result_df['item_count'].fillna(0)
-    
-    # Split the data into two parts
-    train_data = result_df[result_df['checkout_value'].notnull()]
-    missing_data = result_df[result_df['checkout_value'].isnull()]
-
-    # Features and target variable for training
-    X_train = train_data[['days_since_prior_order', 'item_count']]
-    y_train = train_data['checkout_value']
+    if user_df.empty:
+        # Features and target variable for training
+        X_train = train_data[['number_of_items', 'days_since_prior_order']]
+        y_train = train_data['checkout_value']
+    else:
+        X_train = user_df[['number_of_items', 'days_since_prior_order']]
+        y_train = user_df['checkout_value']
 
     # Features for prediction
-    X_missing = pd.DataFrame([[days_since_prior_order, number_of_items]],
-                           columns=['days_since_prior_order', 'item_count'])
+    X_missing = pd.DataFrame([[number_of_items, days_since_prior_order]],
+                           columns=['number_of_items', 'days_since_prior_order'])
 
     # Train the linear regression model
     model = LinearRegression()
@@ -92,6 +93,8 @@ def predict_checkout_value(user_id: int, number_of_items: int, days_since_prior_
     predicted_value = model.predict(X_missing)[0]
     print("predicted checkout_value:", round(predicted_value))
 
-    plot(model, train_data, X_missing, result_df, predicted_value)
+    # uncoment below line if you want to visualise the model
+    # plot(model, train_data, X_missing, result_df, predicted_value)
+    return model
 
 predict_checkout_value(93942, 0, 0)
