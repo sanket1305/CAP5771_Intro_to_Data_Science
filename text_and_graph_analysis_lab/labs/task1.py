1d1924e9000e858b7bcf0cb54d1f25a1506edc66
sanket1305 <meetsanket24@gmail.com>
2025-04-16 21:00:33 -0400

    lab4 task1 done

---
+++ text_and_graph_analysis_lab/labs/task1.py
@@ -0,0 +1,29 @@
+from sklearn.datasets import load_files
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+def train_sentiment_model():
+    container_path = "data/movie_reviews"
+    data = load_files(container_path)
+
+    # split the data and target variables
+    X_train, y_train = data.data, data.target
+    
+    # cretae a pipeline for logistic regression, 
+    # add labels for process for quick access later on
+    pipeline = Pipeline([
+        ('tfidf', TfidfVectorizer(
+            lowercase = True,
+            stop_words = 'english',
+            max_features = 1000,
+            ngram_range = (1, 2)
+        )),
+        ('clf', LogisticRegression(max_iter = 1000))
+    ])
+
+    # train the model
+    pipeline.fit(X_train, y_train)
+    return pipeline
+
+train_sentiment_model()
